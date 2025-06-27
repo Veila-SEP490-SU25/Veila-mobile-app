f05a270ae0144139a8561d70d2e9cbb2322ca718
HaoPhan205 <130329315+HaoPhan205@users.noreply.github.com>
2025-06-27 13:19:16 +0700

    set up

---
+++ app/_layout.tsx
@@ -0,0 +1,20 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+
+import "react-native-reanimated";
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
